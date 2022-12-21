242249d1e9b07e8d0998d18da1e0883530e49e3c
Charlemagne44 <collinhay44@gmail.com>
2022-12-21 15:15:25 -0500

    milestones

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+/*
+1.Build a data structure to store the board state
+2.“Pretty-print” the board to the terminal
+3.Given a starting board state, calculate the next one
+4.Run the game forever
+5. Save interesting starting positions to files and add the ability to reload them into your Life
+6. Improve the User Interface
+7. Change the rules of Life
+*/
+
 func main() {
 	fmt.Println("game of life")
 }
